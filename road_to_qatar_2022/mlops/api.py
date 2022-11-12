699535dd7a32547f1c15c33c60fa094cdbf81601
Jean-Marc Clement <jmclement16@gmail.com>
2022-11-12 18:48:31 +0400

    Defined new api endpoints that might be used

--- road_to_qatar_2022/mlops/api.py
+++ road_to_qatar_2022/mlops/api.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class SelectedTeam(BaseModel):
+    name: str
 
 app = FastAPI()
 
 @app.get('/')
 def index():
     return {'ok':True}
+
+@app.get('/winner')
+def getWinner():
+    return {'Res':'Winner called'}
+
+@app.post('/selected_team')
+def setPreferedTeam(param:SelectedTeam):
+    return {'Post':f'{param.name}'}
+
+@app.post('/update_predictions')
+def updatePredictions():
+    return {'Called update predictions'}
